a035950c3b15f4caa23666c863a9376ba55c7ad5
radiobaek <radiobaek@opensg.net>
2015-04-23 14:49:49 +0900

    04/23

--- net.opensg.tcs.multiedit/src/net/opensg/tcs/multiedit/app/ApplicationActionBarAdvisor.java
+++ net.opensg.tcs.multiedit/src/net/opensg/tcs/multiedit/app/ApplicationActionBarAdvisor.java
@@ -1,14 +1,9 @@
 package net.opensg.tcs.multiedit.app;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.Arrays;
 import java.util.List;
 
-import net.opensg.tcs.commons.libs.core.TreeItemInfo;
 import net.opensg.tcs.main.action.ClearTableViewerAction;
 import net.opensg.tcs.main.action.PreferenceDialogAction;
 import net.opensg.tcs.main.action.SamplePopupAction;
@@ -18,27 +13,32 @@ import net.opensg.tcs.multiedit.io.ContRepository;
 import net.opensg.tcs.multiedit.util.GeneralUtil;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ICoolBarManager;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.ToolBarContributionItem;
+import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
-	private static String filePath = "C:\\test.bin";
-	
 	public static IActionBarConfigurer CurrentActionConfig;
 
 	public SamplePopupAction samplePopupAction;
 	public PreferenceDialogAction prefDialogAction;
+	private Action fileOpenAction;
+	private Action fileSaveAction;
 
 	public MenuManager mainMenu_General;
 
@@ -63,40 +63,58 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	protected void fillMenuBar(IMenuManager menuBar) {
 		// MenuBar - General
 		mainMenu_General = new MenuManager("General", "General");
-		
-		Action File_Open = new Action("Read from File") {
+
+		fileOpenAction = new Action("Read from File") {
 			@Override
 			public void run() {
 				try {
-					FileInputStream fis = new FileInputStream(filePath);
-					ObjectInputStream ois = new ObjectInputStream(fis);
-					ContRepository.modelData = (List<TreeItemInfo>)ois.readObject();
-					ois.close();
+					FileDialog fileDialog = new FileDialog(
+							GeneralUtil.getCurrentShell(), SWT.OPEN);
+					fileDialog.setText("Select Data File");
+					// fileDialog.setFilterPath("C:/");
+					String[] filterExt = { "*.bin", "*.*" };
+					fileDialog.setFilterExtensions(filterExt);
+					String filePath = fileDialog.open();
+					if (filePath != "") {
+						ContRepository.getInstance().readData(filePath);
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				super.run();
 			}
 		};
-		mainMenu_General.add(File_Open);
-		
-		Action File_Save = new Action("Write to File") {
+		mainMenu_General.add(fileOpenAction);
+
+		fileSaveAction = new Action("Write to File") {
 			@Override
 			public void run() {
 				FileOutputStream fos;
 				try {
-					fos = new FileOutputStream(filePath);
-					ObjectOutputStream oos = new ObjectOutputStream(fos);
-					oos.writeObject(ContRepository.modelData);
-					oos.close();
+					String filePath = ContRepository.getInstance()
+							.getModelFilePath();
+					if (filePath == "") {
+						FileDialog fileDialog = new FileDialog(
+								GeneralUtil.getCurrentShell(), SWT.SAVE);
+						fileDialog
+								.setFilterNames(new String[] { "Data Files (*.bin)" });
+						fileDialog
+								.setFilterExtensions(new String[] { "*.bin" });
+						fileDialog.setText("Select Data File to save");
+						// fileDialog.setFilterPath(folder.getRawLocation().toString());
+						filePath = fileDialog.open();
+					}
+					if (filePath != "") {
+						ContRepository.getInstance().writeData(filePath);
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				super.run();
 			}
 		};
-		mainMenu_General.add(File_Save);
-		
+		mainMenu_General.add(fileSaveAction);
+
 		mainMenu_General.add(new Separator());
 		mainMenu_General.add(samplePopupAction);
 		mainMenu_General.add(prefDialogAction);
@@ -107,69 +125,107 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 		mainMenu_TableViewer.add(mainMenuAction_ClearTableViewer);
 		mainMenu_TableViewer.add(new Separator());
 
-		Action TableViewer_BindingOption_Normal = new Action("BindingOption : Normal") {
+		Action TableViewer_BindingOption_Normal = new Action(
+				"BindingOption : Normal") {
 			@Override
 			public void run() {
-				GeneralUtil.setPreferenceString(PreferenceConstants.KEY_TableViewerBindingType, PreferenceConstants.VAL_TableViewerBindingType_Normal);
+				GeneralUtil.setPreferenceString(
+						PreferenceConstants.KEY_TableViewerBindingType,
+						PreferenceConstants.VAL_TableViewerBindingType_Normal);
 				super.run();
 			}
 		};
 		mainMenu_TableViewer.add(TableViewer_BindingOption_Normal);
 
-		Action tableViewer_BindingOption_OwnerDraw = new Action("BindingOption : OwnerDraw") {
+		Action tableViewer_BindingOption_OwnerDraw = new Action(
+				"BindingOption : OwnerDraw") {
 			@Override
 			public void run() {
-				GeneralUtil.setPreferenceString(PreferenceConstants.KEY_TableViewerBindingType, PreferenceConstants.VAL_TableViewerBindingType_OwnerDraw);
+				GeneralUtil
+						.setPreferenceString(
+								PreferenceConstants.KEY_TableViewerBindingType,
+								PreferenceConstants.VAL_TableViewerBindingType_OwnerDraw);
 				super.run();
 			}
 		};
 		mainMenu_TableViewer.add(tableViewer_BindingOption_OwnerDraw);
-		
-		Action TableViewer_BindingOption_ToolTip = new Action("BindingOption : ToolTip Column") {
+
+		Action TableViewer_BindingOption_ToolTip = new Action(
+				"BindingOption : ToolTip Column") {
 			@Override
 			public void run() {
-				GeneralUtil.setPreferenceString(PreferenceConstants.KEY_TableViewerBindingType, PreferenceConstants.VAL_TableViewerBindingType_ToolTipColumn);
+				GeneralUtil
+						.setPreferenceString(
+								PreferenceConstants.KEY_TableViewerBindingType,
+								PreferenceConstants.VAL_TableViewerBindingType_ToolTipColumn);
 				super.run();
 			}
 		};
 		mainMenu_TableViewer.add(TableViewer_BindingOption_ToolTip);
 
-		Action TableViewer_BindingOption_ShowHideColumn = new Action("BindingOption : ShowHide Column") {
+		Action TableViewer_BindingOption_ShowHideColumn = new Action(
+				"BindingOption : ShowHide Column") {
 			@Override
 			public void run() {
-				GeneralUtil.setPreferenceString(PreferenceConstants.KEY_TableViewerBindingType, PreferenceConstants.VAL_TableViewerBindingType_ShowHideColumn);
+				GeneralUtil
+						.setPreferenceString(
+								PreferenceConstants.KEY_TableViewerBindingType,
+								PreferenceConstants.VAL_TableViewerBindingType_ShowHideColumn);
 				super.run();
 			}
 		};
 		mainMenu_TableViewer.add(TableViewer_BindingOption_ShowHideColumn);
 
 		mainMenu_TableViewer.add(new Separator());
-		
-		Action TableViewer_BindingOption_ComboCellEditor = new Action("BindingOption : Combo CellEditor", SWT.CHECK) {
+
+		Action TableViewer_BindingOption_ComboCellEditor = new Action(
+				"BindingOption : Combo CellEditor", SWT.CHECK) {
 			@Override
 			public void runWithEvent(Event event) {
-				boolean checked_ComboCellEditor = GeneralUtil.getPreferenceBool(PreferenceConstants.KEY_TableViewerOption_ComboCellEditor);
-				GeneralUtil.setPreferenceBool(PreferenceConstants.KEY_TableViewerOption_ComboCellEditor, !checked_ComboCellEditor);
+				boolean checked_ComboCellEditor = GeneralUtil
+						.getPreferenceBool(PreferenceConstants.KEY_TableViewerOption_ComboCellEditor);
+				GeneralUtil
+						.setPreferenceBool(
+								PreferenceConstants.KEY_TableViewerOption_ComboCellEditor,
+								!checked_ComboCellEditor);
 				super.runWithEvent(event);
 			}
 		};
-		boolean checked_ComboCellEditor = GeneralUtil.getPreferenceBool(PreferenceConstants.KEY_TableViewerOption_ComboCellEditor);
-		TableViewer_BindingOption_ComboCellEditor.setChecked(checked_ComboCellEditor);
+		boolean checked_ComboCellEditor = GeneralUtil
+				.getPreferenceBool(PreferenceConstants.KEY_TableViewerOption_ComboCellEditor);
+		TableViewer_BindingOption_ComboCellEditor
+				.setChecked(checked_ComboCellEditor);
 		mainMenu_TableViewer.add(TableViewer_BindingOption_ComboCellEditor);
-		
-		Action TableViewer_BindingOption_CellEditorPerRow = new Action("BindingOption : CellEditor Per Row", SWT.CHECK) {
+
+		Action TableViewer_BindingOption_CellEditorPerRow = new Action(
+				"BindingOption : CellEditor Per Row", SWT.CHECK) {
 			@Override
 			public void runWithEvent(Event event) {
-				boolean checked_CellEditorPerRow = GeneralUtil.getPreferenceBool(PreferenceConstants.KEY_TableViewerOption_CellEditorPerRow);
-				GeneralUtil.setPreferenceBool(PreferenceConstants.KEY_TableViewerOption_CellEditorPerRow, !checked_CellEditorPerRow);
+				boolean checked_CellEditorPerRow = GeneralUtil
+						.getPreferenceBool(PreferenceConstants.KEY_TableViewerOption_CellEditorPerRow);
+				GeneralUtil
+						.setPreferenceBool(
+								PreferenceConstants.KEY_TableViewerOption_CellEditorPerRow,
+								!checked_CellEditorPerRow);
 				super.runWithEvent(event);
 			}
 		};
-		boolean checked_CellEditorPerRow = GeneralUtil.getPreferenceBool(PreferenceConstants.KEY_TableViewerOption_CellEditorPerRow);
-		TableViewer_BindingOption_CellEditorPerRow.setChecked(checked_CellEditorPerRow);
+		boolean checked_CellEditorPerRow = GeneralUtil
+				.getPreferenceBool(PreferenceConstants.KEY_TableViewerOption_CellEditorPerRow);
+		TableViewer_BindingOption_CellEditorPerRow
+				.setChecked(checked_CellEditorPerRow);
 		mainMenu_TableViewer.add(TableViewer_BindingOption_CellEditorPerRow);
-		
+
 		menuBar.add(mainMenu_TableViewer);
+	}
+
+	@Override
+	protected void fillCoolBar(ICoolBarManager coolBar) {
+		//super.fillCoolBar(coolBar);
+        IToolBarManager toolbar = new ToolBarManager(SWT.FLAT | SWT.RIGHT);
+        coolBar.add(new ToolBarContributionItem(toolbar, "main"));   
+        toolbar.add(fileOpenAction);
+        toolbar.add(fileSaveAction);
 	}
 
 	@Override
--- net.opensg.tcs.multiedit/src/net/opensg/tcs/multiedit/views/NaviView.java
+++ net.opensg.tcs.multiedit/src/net/opensg/tcs/multiedit/views/NaviView.java
@@ -2,6 +2,8 @@ package net.opensg.tcs.multiedit.views;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 import net.opensg.tcs.commons.libs.core.TcsCommon;
 import net.opensg.tcs.commons.libs.core.TreeItemInfo;
@@ -26,24 +28,55 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class NaviView extends ViewPart {
 
 	public static String ID = "net.opensg.tcs.multiedit.views.NaviView";
+	
 	private TreeViewer treeViewer;
+	private Text txtFilePath;
 
 	public NaviView() {
 	}
 
 	public void createPartControl(Composite parent) {
+		GridLayout gl_parent = new GridLayout(1, false);
+		gl_parent.verticalSpacing = 0;
+		parent.setLayout(gl_parent);
+		
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout gl_composite = new GridLayout(2, false);
+		gl_composite.verticalSpacing = 0;
+		gl_composite.marginWidth = 0;
+		gl_composite.marginHeight = 0;
+		composite.setLayout(gl_composite);
+		GridData gd_composite = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		gd_composite.heightHint = 30;
+		composite.setLayoutData(gd_composite);
+		
+		CLabel lblFilePath = new CLabel(composite, SWT.NONE);
+		lblFilePath.setLayoutData(new GridData(SWT.LEFT, SWT.FILL, false, true, 1, 1));
+		lblFilePath.setText("File Path:");
+		
+		txtFilePath = new Text(composite, SWT.BORDER | SWT.READ_ONLY);
+		txtFilePath.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		txtFilePath.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
 		Tree tree = new Tree(parent, SWT.BORDER);
+		tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		treeViewer = new TreeViewer(tree);
 
 		treeViewer.setContentProvider(new NaviViewContentProvider());
 		treeViewer.setLabelProvider(new NaviViewLabelProvider());
 
-		ContRepository.modelData = TreeViewerBindingData();
-		treeViewer.setInput(ContRepository.modelData);
+		ContRepository.getInstance().setModelData(TreeViewerBindingData());
+		treeViewer.setInput(ContRepository.getInstance().getModelData());
 
 		// Selection service 지정
 		getSite().setSelectionProvider(treeViewer);
@@ -84,7 +117,6 @@ public class NaviView extends ViewPart {
 			@Override
 			public void treeExpanded(TreeExpansionEvent event) {
 			}
-
 			@Override
 			public void treeCollapsed(TreeExpansionEvent event) {
 			}
@@ -100,7 +132,22 @@ public class NaviView extends ViewPart {
 			}
 		});
 
-
+		// Data Observer 지정
+		ContRepository.getInstance().addObserver(new Observer() {
+			@Override
+			public void update(Observable arg0, Object arg1) {
+				String changeReason = (String)arg1;
+				switch (changeReason) {
+				case ContRepository.dataChangeReason_ModelData:
+					NaviView.this.bindData_TreeViewer();
+					break;
+				case ContRepository.dataChangeReason_FilePath:
+					NaviView.this.bindData_FilePath();
+					break;
+				}
+			}
+		});
+		
 	}
 
 	public void setFocus() {
@@ -116,4 +163,15 @@ public class NaviView extends ViewPart {
 		}
 		return resultList;
 	}
+	
+	
+	
+	public void bindData_TreeViewer() {
+		System.out.println("NaviView bindData");
+		this.treeViewer.setInput(ContRepository.getInstance().getModelData());
+	}
+	public void bindData_FilePath() {
+		this.txtFilePath.setText(ContRepository.getInstance().getModelFilePath());
+	}
+	
 }
